99e27b84a4e2243ede4c1c54572ef85d2b550d6c
Kohei Hasegawa <ameutau@gmail.com>
2017-04-09 19:10:13 +0900

    Update rule for Jest

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,8 +3,15 @@ module.exports = {
   "plugins": [
     "react",
     "jsx-a11y",
-    "import"
+    "import",
+    "jest"
   ],
+  "env": {
+    "jest/globals": true
+  },
+  "globals": {
+    "context": true,
+  },
   "rules": {
     semi: [2, "never"],
   }
