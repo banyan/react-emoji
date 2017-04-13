b12939d06d5e6aeb15106a97882f351eb78fd500
Kohei Hasegawa <ameutau@gmail.com>
2017-04-13 18:35:52 +0900

    .

--- example/src/index.js
+++ example/src/index.js
@@ -3,6 +3,9 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import App from './App'
 
+import injectTapEventPlugin from 'react-tap-event-plugin'
+injectTapEventPlugin()
+
 ReactDOM.render(
   <App />,
   document.getElementById('root')
